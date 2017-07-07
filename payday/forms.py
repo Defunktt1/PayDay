780aa442cab1c9b4f5a9356cd3fa0f741da37694
defunktt1 <vurado3@gmail.com>
2017-07-07 12:25:56 +0300

    Count money

--- payday/forms.py
+++ payday/forms.py
@@ -21,5 +21,7 @@ class EntryForm(forms.ModelForm):
 class CountForm(forms.Form):
     from_date = forms.DateField(label="От:", widget=SelectDateWidget)
     to_date = forms.DateField(label="До:", widget=SelectDateWidget)
-    hour_rate = forms.FloatField(label="Зарплата за час:", min_value=0.0, )
-    company_rate = forms.FloatField(label="Сколько забирает фирма:", min_value=0.0)
+    hour_rate = forms.FloatField(label="Зарплата за час:", min_value=0.0)
+    company_rate = forms.FloatField(label="Сколько забирает фирма (в процентах):", min_value=0.0)
+    manager_rate = forms.FloatField(label="Сколько забирает менеджер (в процентах):", min_value=0.0)
+    exchange_rates = forms.FloatField(label="Курс гривны:", min_value=0.0)
--- payday/views.py
+++ payday/views.py
@@ -44,18 +44,66 @@ def count(request):
         form = CountForm(request.POST)
 
         if form.is_valid():
-            count_hours = 0
-            from_date = form.cleaned_data.get('from_date')
-            to_date = form.cleaned_data.get('to_date')
-            data = Entry.objects.filter(day__gte=from_date).filter(day__lte=to_date)
+            request_data = form.cleaned_data
 
-            if data:
-                for hours in data:
-                    count_hours += hours.hours
+            # basic response
+            response = {
+                "form": form,
+            }
 
-            result = count_hours * float(form.get('hour_rate')) * float(form.get('company_rate'))
-            return render(request, 'payday/count.html', {'form': form,
-                                                         'result': result})
+            # get date interval between two dates
+            from_date = request_data['from_date']
+            to_date = request_data['to_date']
+            date_interval = Entry.objects.filter(day__gte=from_date).filter(day__lte=to_date)
+
+            # check if data is valid
+            date_is_valid = from_date <= to_date
+            if not date_is_valid:
+                response["wrong"] = "Первая дата должна быть меньше второй"
+
+            # count hours
+            total_hours = 0
+            check_hours = date_interval.exists()
+            if not check_hours:
+                response["wrong"] = "Вы не работали в эти дни"
+
+            if date_is_valid and check_hours:
+                for hours in date_interval:
+                    total_hours += hours.hours
+
+                # set data from request to variables
+                hour_rate = request_data['hour_rate']
+                exchange_rates = request_data['exchange_rates']
+                manager_rate = request_data['manager_rate']
+                company_rate = request_data['company_rate']
+
+                # count operations
+                total_money_in_usd = total_hours * hour_rate
+                total_money_in_uan = total_money_in_usd * exchange_rates
+                manager_result_in_usd = total_money_in_usd * (manager_rate / 100)
+                company_result_in_usd = total_money_in_usd * (company_rate / 100)
+                user_result_in_usd = total_money_in_usd - manager_result_in_usd - company_result_in_usd
+                manager_result_in_uan = manager_result_in_usd * exchange_rates
+                company_result_in_uan = company_result_in_usd * exchange_rates
+                user_result_in_uan = user_result_in_usd * exchange_rates
+
+                # set all data to dict
+                total_result = {
+                    "total_money_in_usd": total_money_in_usd,
+                    "total_money_in_uan": total_money_in_uan,
+                    "manager_result_in_usd": manager_result_in_usd,
+                    "manager_result_in_uan": manager_result_in_uan,
+                    "company_result_in_usd": company_result_in_usd,
+                    "company_result_in_uan": company_result_in_uan,
+                    "user_result_in_usd": user_result_in_usd,
+                    "user_result_in_uan": user_result_in_uan,
+                }
+
+                # update response dict
+                response.update(total_result)
+
+            return render(request, 'payday/count.html', context=response)
+
     else:
         form = CountForm()
 
