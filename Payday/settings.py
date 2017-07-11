4177fdaab4f4d8af001c9d7ed805deb1296c624f
defunktt1 <vurado3@gmail.com>
2017-07-11 14:21:03 +0300

    heroku

--- Payday/settings.py
+++ Payday/settings.py
@@ -116,6 +116,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-STATICFILES_DIR = [STATIC_DIR, ]
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
+
 STATIC_ROOT = 'staticfiles'
