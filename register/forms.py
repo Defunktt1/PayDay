2fef916c3566c1fb13229d54d4b54752a19e0b04
defunktt1 <vurado3@gmail.com>
2017-07-11 13:04:24 +0300

    Added registration, login and logout

--- register/forms.py
+++ register/forms.py
@@ -26,20 +26,72 @@ class RegisterForm(forms.ModelForm):
 
     def clean_username(self):
         username = self.cleaned_data.get("username")
+        len_username = len(username)
+
+        if 5 > len_username < 30:
+            raise forms.ValidationError("Неправильное количество символов")
+
         if User.objects.filter(username=username).exists():
             raise forms.ValidationError("Имя пользователя занято")
+
         return username
+
+    def clean_password(self):
+        password = self.cleaned_data.get("password")
+        len_password = len(password)
+
+        if 5 > len_password < 30:
+            raise forms.ValidationError("Неправильное количество символов")
+
+        return password
 
     def clean_confirm_password(self):
         user_password = self.cleaned_data.get("password")
         confirm_password = self.cleaned_data.get("confirm_password")
+        len_confirm_password = len(confirm_password)
+
+        if 5 > len_confirm_password < 30:
+            raise forms.ValidationError("Неправильное количество символов")
 
         if user_password != confirm_password:
             raise forms.ValidationError("Пароли не совпадают")
+
         return user_password
 
     def clean_email(self):
         email = self.cleaned_data.get("email")
+        len_email = len(email)
+
         if User.objects.filter(email=email).exists():
             raise forms.ValidationError("Этот email уже занят")
+
+        if 4 > len_email < 30:
+            raise forms.ValidationError("Неправильное количество символов")
+
         return email
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField(label="Имя пользователя")
+    password = forms.CharField(label="Пароль", widget=forms.PasswordInput())
+
+    def clean_username(self):
+        username = self.cleaned_data.get("username")
+        len_username = len(username)
+
+        if not User.objects.filter(username=username).exists():
+            raise forms.ValidationError("Такой логин не дайден")
+
+        if 2 > len_username < 30:
+            raise forms.ValidationError("Неправильное количество символов")
+
+        return username
+
+    def clean_password(self):
+        password = self.cleaned_data.get("password")
+        len_password = len(password)
+
+        if 5 > len_password < 30:
+            raise forms.ValidationError("Неправильное количество символов")
+
+        return password
--- register/views.py
+++ register/views.py
@@ -1,25 +1,45 @@
-from django.shortcuts import render
-from .forms import RegisterForm
+from django.shortcuts import render, HttpResponseRedirect
+from .forms import RegisterForm, LoginForm
 from django.contrib.auth.models import User
-from django.core.exceptions import ValidationError
 from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 
 
-def register(request):
+def user_register(request):
     if request.method == "POST":
         form = RegisterForm(request.POST)
         if form.is_valid():
-            try:
-                user_name = form.clean_username()
-                user_email = form.clean_email()
-                user_password = form.clean_confirm_password()
-                user = User.objects.create_user(user_name, user_email, user_password)
-                user.save()
-
-            except ValidationError as ve:
-                messages.add_message(request, messages.ERROR, ve)
-
+            user_name = form.clean_username()
+            user_email = form.clean_email()
+            user_password = form.clean_confirm_password()
+            user = User.objects.create_user(user_name, user_email, user_password)
+            user.save()
+            messages.success(request, "Регистрация прошла успешно!")
+            return HttpResponseRedirect("/")
     else:
         form = RegisterForm()
 
     return render(request, 'register/register.html', {"form": form})
+
+
+def user_login(request):
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.clean_username()
+            password = form.clean_password()
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.success(request, "Добро пожаловать, {0}".format(username))
+                return HttpResponseRedirect("/")
+    else:
+        form = LoginForm()
+
+    return render(request, 'register/login.html', {"form": form})
+
+
+def user_logout(request):
+    logout(request)
+    messages.success(request, "До свидания!")
+    return HttpResponseRedirect("/")
